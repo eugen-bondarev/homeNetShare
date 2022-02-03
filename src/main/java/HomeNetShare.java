7cc3e52116106e42c021d6e9c4c533090b7aeb75
eugen-bondarev <eug.bondarev@gmail.com>
2022-02-03 12:19:30 +0100

    Window creation

--- src/main/java/HomeNetShare.java
+++ src/main/java/HomeNetShare.java
@@ -1,5 +1,26 @@
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+
 public class HomeNetShare {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try {
+            if (!glfwInit()) {
+                throw new Exception("Failed to load glfw.");
+            }
+            glfwDefaultWindowHints();
+            long handle = glfwCreateWindow(800, 600, "Hello, world!", 0, 0);
+            glfwMakeContextCurrent(handle);
+
+            GL.createCapabilities();
+
+            while (!glfwWindowShouldClose(handle)) {
+                glfwPollEvents();
+                glfwSwapBuffers(handle);
+            }
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
